bce60c810e299eae75260a47e0f7c49f4dc0b276
Mateusz Kreczmer <mateusz.kreczmer7@gmail.com>
2025-12-15 14:48:44 +0100

    chore(config): update app settings loading

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -6,8 +6,9 @@ class Settings(BaseSettings):
     database_url: str
     secret_key: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+    )
 
 
 settings = Settings()
