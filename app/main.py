2426a7d0eea7aa0ab8ab6cb735d864aaeead9d3d
Mateusz Kreczmer <mateusz.kreczmer7@gmail.com>
2025-12-15 14:48:08 +0100

    feat(api): add auth router into app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+from app.api.v1 import auth_router
+
 app = FastAPI(
     title="Subscription API",
     version="0.1.0",
 )
 
+app.include_router(auth_router)
 
 @app.get("/", tags=["root"])
 async def root() -> dict:
